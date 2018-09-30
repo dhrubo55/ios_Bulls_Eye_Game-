34f4f1828525ab3b1878a0a9d9042d53e7fba0de
Dhrubo Hasan <dhrubohasan@dhrubos-mac.local>
2018-09-30 13:32:00 -0700

    Slider is working of the bull's eye game slider value is in alert controller

--- HitMeGame/ViewController.swift
+++ HitMeGame/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func showMe(){
+        let message = "The value of the slider is now \(currentValue)"
+        
+       let alert = UIAlertController(title: "Hello world", message: message, preferredStyle:.alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert,animated: true,completion: nil)
+    }
+    
+    
+    @IBAction func sliderMove(_ slider: UISlider){
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
 }
 
